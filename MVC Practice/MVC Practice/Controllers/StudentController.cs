1900066fd72227786faf2f6a8000b0d41dc2e162
Farshe-Salman <salmanfarshe3071@gmail.com>
2025-11-09 22:31:40 +0600

    Add some new controller , view and data passing by view data

---
+++ MVC Practice/MVC Practice/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Practice.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            ViewBag.S1F = "Kalman";
+            ViewBag.S1L = "Dalman";
+            ViewBag.S1E = "Kalman@gmail.com";
+            ViewBag.S2F = "jalman";
+            ViewBag.S2L = "lalman";
+            ViewBag.S2E = "jalman@gmail.com";
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();   
+        }
+    }
+}
