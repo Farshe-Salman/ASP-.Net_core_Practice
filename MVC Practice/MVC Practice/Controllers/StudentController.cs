57e7886b7e32011a2b9b20181eb15a7822b1130c
Farshe-Salman <salmanfarshe3071@gmail.com>
2025-11-09 23:54:02 +0600

    Add more controller, view. And data passing  by view bag, temp data (show pop up message), modelbinding (by new class in model), Also Redirect to internal link (in same and different controller) and external link.

--- MVC Practice/MVC Practice/Controllers/StudentController.cs
+++ MVC Practice/MVC Practice/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC_Practice.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,18 +12,67 @@ namespace MVC_Practice.Controllers
         // GET: Student
         public ActionResult Index()
         {
+            //viewbag
             ViewBag.S1F = "Kalman";
             ViewBag.S1L = "Dalman";
             ViewBag.S1E = "Kalman@gmail.com";
+
             ViewBag.S2F = "jalman";
             ViewBag.S2L = "lalman";
             ViewBag.S2E = "jalman@gmail.com";
+
+            //viewdata
+            ViewData["S3F"] = "Palman";
+            ViewData["S3L"] = "Talman";
+            ViewData["S3E"] = "palman@gmail.com";
             return View();
         }
 
         public ActionResult Create()
         {
             return View();   
+        }
+
+        public ActionResult Login()
+        {
+            if(true) 
+            {
+                //Temp data
+                TempData["Message"] = "create a new account";
+
+                //redirect to internal link in same controller
+                return RedirectToAction("Create");  
+            }
+        }
+
+        public ActionResult HomeIndex()
+        {
+             if(true)
+            {
+                //redirect to internal link in different controller
+                return RedirectToAction("Index", "Home");  
+            }
+        }
+
+        public ActionResult Aiub()
+        {
+            if(true)
+            {
+                return Redirect("https://www.aiub.edu");    //redirect to external link
+            }
+        }
+
+        public ActionResult Details()
+        {
+            //modelBinding
+            var s1 = new Student()
+            {
+                FirstName = "Kalman",
+                LastName = "Dalman",
+                Email = "Kalman@gmail.com"
+            };
+
+            return View(s1);
         }
     }
 }
