ee74a99e4d448fc39c7a2021ed8aaa3893952397
Farshe-Salman <salmanfarshe3071@gmail.com>
2025-11-15 01:19:46 +0600

    Add a new Entity Framework Project. Connect with My SQL sserver, Fetch a table from the server and show the data in a web page. Also insert data in a table bu a form.

---
+++ EFIntro/EFIntro/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using EFIntro.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFIntro.Controllers
+{
+    public class StudentController : Controller
+    {
+        UniversityDBEntities db = new UniversityDBEntities();
+        public ActionResult Index()
+        {
+            var data=db.Students.ToList();
+            return View(data);
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            db.Students.Add(student);
+            db.SaveChanges();
+            TempData["msg"] = "Student Created Successfully";
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
